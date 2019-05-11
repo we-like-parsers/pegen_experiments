ebe38e303fbb42b5ff3a8bfc728ef5f2f2e2c0a7
Guido van Rossum <guido@python.org>
2019-05-11 15:48:10 -0700

    Refactor test

--- test_pegen.py
+++ test_pegen.py
@@ -4,13 +4,7 @@ import tokenize
 from pegen import GrammarParser, ParserGenerator, Tokenizer, Tree
 
 
-def test_expr_grammar():
-    # Read the expression grammar.
-    with open('expr.txt') as file:
-        tokenizer = Tokenizer(tokenize.generate_tokens(file.readline))
-        parser = GrammarParser(tokenizer)
-        tree = parser.start()
-
+def generate_parser(tree):
     # Generate a parser.
     genr = ParserGenerator(tree)
     out = io.StringIO()
@@ -20,13 +14,27 @@ def test_expr_grammar():
     # Load the generated parser class.
     ns = {}
     exec(out.getvalue(), ns)
-    GeneratedParser = ns['GeneratedParser']
+    return ns['GeneratedParser']
 
+
+def run_parser(file, parser_class):
     # Run the parser.
-    file = io.StringIO("42\n")
     tokenizer = Tokenizer(tokenize.generate_tokens(file.readline))
-    parser = GeneratedParser(tokenizer)
-    tree = parser.start()
+    parser = parser_class(tokenizer)
+    return parser.start()
+
+
+def test_expr_grammar():
+    # Read the expression grammar.
+    with open('expr.txt') as file:
+        tree = run_parser(file, GrammarParser)
+
+    # Generate the parser and load the class.
+    parser_class = generate_parser(tree)
+
+    # Parse sample input.
+    file = io.StringIO("42\n")
+    tree = run_parser(file, parser_class)
 
     # Check the tree.
     assert tree == Tree('start', Tree('sums',
@@ -37,3 +45,4 @@ def test_expr_grammar():
                                       Tree('Empty')))
 
     # TODO: Verify other inputs.
+
