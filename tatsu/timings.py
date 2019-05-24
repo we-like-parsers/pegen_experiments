f1bbb6f76a30f14ea1a7bb5050766d7615806b89
Guido van Rossum <guido@python.org>
2019-05-24 11:36:41 -0700

    Add a timing test with TatSu
    To run:
    
    pip3 install tatsu
    tatsu grammar.ebnf --whitespace ' ' >parse.py
    python3 timings.py ../medium.txt
    
    Timing output:
    
    100 lines in 6.199 secs
    16 lines/sec
    Memory stats:
      rss         :        122 MiB
      vms         :       4313 MiB
      maxrss      :        122 MiB
    
    Concluding, TatSu's Python parser is much slower than my toy parser
    (16 lines/sec vs. 600 line/sec, i.e. ~35x).  Then again, the native
    parser is 25x faster than my toy parse.

---
+++ tatsu/timings.py
@@ -0,0 +1,32 @@
+import sys
+import time
+
+sys.path.append('..')
+import pegen
+
+from tatsu.util import generic_main
+from parse import UnknownBuffer, UnknownParser
+
+def main():
+    t0 = time.time()
+    for filename in sys.argv[1:]:
+        try:
+            with open(filename) as file:
+                text = file.read()
+            parser = UnknownParser()
+            ast = parser.parse(text, rule_name='start', filename=filename, whitespace=' ')
+        except Exception as err:
+            print("Error:", err, file=sys.stderr)
+    t1 = time.time()
+    nlines = 0
+    for filename in sys.argv[1:]:
+        with open(filename) as file:
+            nlines += len(file.readlines())
+    dt = t1 - t0
+    print(f"{nlines} lines in {dt:.3f} secs", file=sys.stderr)
+    if dt:
+        print(f"{nlines/dt:.0f} lines/sec", file=sys.stderr)
+    pegen.print_memstats()
+
+if __name__ == '__main__':
+    main()
