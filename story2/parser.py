d98c9d9c4175c0d99c40988d1241eea36a72fc65
Guido van Rossum <guido@python.org>
2019-07-30 09:19:40 -0700

    Add a @memoize decorator

--- story2/parser.py
+++ story2/parser.py
@@ -1,7 +1,10 @@
+from story2.memo import memoize
+
 class Parser:
 
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
+        self.memos = {}
 
     def mark(self):
         return self.tokenizer.mark()
@@ -9,6 +12,7 @@ class Parser:
     def reset(self, pos):
         self.tokenizer.reset(pos)
 
+    @memoize
     def expect(self, arg):
         token = self.tokenizer.peek_token()
         if token.type == arg or token.string == arg:
