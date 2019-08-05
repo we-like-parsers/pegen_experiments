aea769b5c123adc307f2e67634c9ee90dee00fef
Pablo Galindo <pablogsal@gmail.com>
2019-08-05 11:48:38 +0100

    Add docstring to the compile_c_extension module

--- pegen/build.py
+++ pegen/build.py
@@ -8,11 +8,21 @@ from distutils.command.build_ext import build_ext
 MOD_DIR = pathlib.Path(__file__)
 
 
-def compile_c_extension(output, build_dir=None):
-    source_file_path = pathlib.Path(output)
+def compile_c_extension(generated_source_path, build_dir=None):
+    """Compile the generated source for a parser generator into an extension module.
+
+    The extension module will be generated in the same directory as the provided path
+    for the generated source, with the same basename (in addition to extension module
+    metadata). For example, for the source mydir/parser.c the generated extension
+    in a darwin system with python 3.8 will be mydir/parser.cpython-38-darwin.so.
+
+    If *build_dir* is provided, that path will be used as the temporary build directory
+    of distutils (this is useful in case you want to use a temporary directory).
+    """
+    source_file_path = pathlib.Path(generated_source_path)
     extension_name = source_file_path.stem
     extension = [Extension(extension_name,
-                           sources=[str(MOD_DIR.parent / "pegen.c"), output],
+                           sources=[str(MOD_DIR.parent / "pegen.c"), generated_source_path],
                            include_dirs=[str(MOD_DIR.parent)],
                            extra_compile_args=[], )]
     dist = Distribution({'name': extension_name, 'ext_modules': extension})
