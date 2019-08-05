231635661f866c95aebb36de4b77b7b87a5c1077
Guido van Rossum <guido@python.org>
2019-08-04 21:33:20 -0700

    Undo silly 'p = print' alias

--- story2/generator.py
+++ story2/generator.py
@@ -14,42 +14,40 @@ from story2.parser import Parser
 
 import sys
 
-p = print  # Shorthand
-
 
 def generate(rules, stream=None):
     if stream:
         sys.stdout = stream
-    p(HEADER)
+    print(HEADER)
     generate_parser_class(rules)
 
 
 def generate_parser_class(rules):
-    p(f"class ToyParser(Parser):")
+    print(f"class ToyParser(Parser):")
     for rule in rules:
-        p()
-        p(f"    @memoize")
-        p(f"    def {rule.name}(self):")
-        p(f"        pos = self.mark()")
+        print()
+        print(f"    @memoize")
+        print(f"    def {rule.name}(self):")
+        print(f"        pos = self.mark()")
         for alt in rule.alts:
             items = []
-            p(f"        if (True")
+            print(f"        if (True")
             for item in alt:
                 if item[0] in ('"', "'"):
-                    p(f"            and self.expect({item})")
+                    print(f"            and self.expect({item})")
                 else:
                     var = item.lower()
                     if var in items:
                         var += str(len(items))
                     items.append(var)
                     if item.isupper():
-                        p("            " +
-                          f"and ({var} := self.expect({item}))")
+                        print("            " +
+                              f"and ({var} := self.expect({item}))")
                     else:
-                        p(f"            " +
-                          f"and ({var} := self.{item}())")
-            p(f"        ):")
-            p(f"            " +
+                        print(f"            " +
+                              f"and ({var} := self.{item}())")
+            print(f"        ):")
+            print(f"            " +
               f"return Node({rule.name!r}, [{', '.join(items)}])")
-            p(f"        self.reset(pos)")
-        p(f"        return None")
+            print(f"        self.reset(pos)")
+        print(f"        return None")
