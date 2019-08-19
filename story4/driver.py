b20024b2b79d1f5bdf8f31090dbb1ff16c41504d
Guido van Rossum <guido@python.org>
2019-08-18 17:33:03 -0700

    Go back to in.txt as default input; add option to set start method

--- story4/driver.py
+++ story4/driver.py
@@ -8,16 +8,20 @@ from story4.visualizer import Visualizer
 
 
 def main():
-    filename = "story4/foo.txt"
+    filename = "story4/in.txt"
+    startname = "start"
     if sys.argv[1:]:
         filename = sys.argv[1]
+        if sys.argv[2:]:
+            startname = sys.argv[2]
     with open(filename) as f:
         tokengen = generate_tokens(f.readline)
         vis = Visualizer()
         tok = Tokenizer(tokengen, vis)
         p = ToyParser(tok)
+        start = getattr(p, startname)
         try:
-            tree = p.statements()
+            tree = start()
             vis.done()
         finally:
             vis.close()
