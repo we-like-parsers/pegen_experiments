53cb49b42cb32f8d61bbe040c4e931d6f897c799
Guido van Rossum <guido@python.org>
2019-09-13 10:35:52 +0100

    Add --short mode to test_parse_directory.py
    Also:
    - Make the script executable
    - Print errors that precede sys.exit(1) to stderr
    - Add a message showing how many errors were found (if > 0)

--- test_parse_directory.py
+++ test_parse_directory.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3.8
+
 import argparse
 import os
 import sys
@@ -18,6 +20,12 @@ argparser.add_argument(
 )
 argparser.add_argument("-g", "--grammar-file", help="Grammar file path")
 argparser.add_argument(
+    "-s",
+    "--short",
+    action="store_true",
+    help="Only show errors, in a more Emacs-friendly format"
+)
+argparser.add_argument(
     "-v",
     "--verbose",
     action="store_true",
@@ -26,7 +34,10 @@ argparser.add_argument(
 )
 
 
-def report_status(succeeded, file, verbose, error=None):
+def report_status(succeeded, file, verbose, error=None, short=False):
+    if short and succeeded:
+        return
+
     if succeeded is True:
         status = "OK"
         COLOR = SUCCESS
@@ -34,10 +45,16 @@ def report_status(succeeded, file, verbose, error=None):
         status = "Fail"
         COLOR = FAIL
 
-    print(f"{COLOR}{file:50} {status}{ENDC}")
+    if short:
+        if isinstance(error, SyntaxError):
+            print(f"{file}:{error.lineno}: error: {error}")
+        else:
+            print(f"{file}: error: {error}")
+    else:
+        print(f"{COLOR}{file:60} {status}{ENDC}")
 
-    if error and verbose:
-        print(f"  {str(error.__class__.__name__)}: {error}")
+        if error and verbose:
+            print(f"  {str(error.__class__.__name__)}: {error}")
 
 
 def main():
@@ -47,18 +64,20 @@ def main():
     verbose = args.verbose
 
     if not directory:
-        print("You must specify a directory of files to test.")
+        print("You must specify a directory of files to test.", file=sys.stderr)
+        sys.exit(1)
 
     if grammar_file:
         if not os.path.exists(grammar_file):
-            print(f"The specified grammar file, {grammar_file}, does not exist.")
+            print(f"The specified grammar file, {grammar_file}, does not exist.", file=sys.stderr)
             sys.exit(1)
 
         try:
             build_parser_and_generator(grammar_file, "pegen/parse.c", True)
         except Exception as err:
             print(
-                f"{FAIL}The following error occurred when generating the parser. Please check your grammar file.\n{ENDC}"
+                f"{FAIL}The following error occurred when generating the parser. Please check your grammar file.\n{ENDC}",
+                file=sys.stderr,
             )
             traceback.print_exception(err.__class__, err, None)
 
@@ -71,13 +90,14 @@ def main():
         from pegen import parse
     except:
         print(
-            "An existing parser was not found. Please run `make` or specify a grammar file with the `-g` flag."
+            "An existing parser was not found. Please run `make` or specify a grammar file with the `-g` flag.",
+            file=sys.stderr,
         )
         sys.exit(1)
 
     # For a given directory, traverse files and attempt to parse each one
     # - Output success/failure for each file
-    success = True
+    errors = 0
     for root, dirs, files in os.walk(directory):
         for file in files:
             # Only attempt to parse Python files
@@ -88,14 +108,17 @@ def main():
 
             try:
                 parse.parse_file(file_path)
-                report_status(succeeded=True, file=file_path, verbose=verbose)
+                if not args.short:
+                    report_status(succeeded=True, file=file_path, verbose=verbose)
             except Exception as error:
                 report_status(
-                    succeeded=False, file=file_path, verbose=verbose, error=error
+                    succeeded=False, file=file_path, verbose=verbose, error=error,
+                    short=args.short
                 )
-                success = False
+                errors += 1
 
-    if not success:
+    if errors:
+        print(f"Encountered {errors} failures.", file=sys.stderr)
         sys.exit(1)
 
 
