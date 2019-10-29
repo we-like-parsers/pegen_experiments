bdabfb617758265ce6c2a9bce6c7ed5130a7aaae
Guido van Rossum <guido@python.org>
2019-10-28 21:14:42 -0700

    Fix mypy errors

--- test/test_ast_generation.py
+++ test/test_ast_generation.py
@@ -1,5 +1,7 @@
 import ast
 import os
+from pathlib import PurePath
+from typing import Any
 
 from pegen.grammar_parser import GeneratedParser as GrammarParser
 from pegen.testutil import parse_string, generate_parser_c_extension
@@ -7,21 +9,21 @@ from pegen.testutil import parse_string, generate_parser_c_extension
 PYTHON_SOURCE_FILENAMES = ["pass.py", "multiple_pass.py"]
 
 
-def create_tmp_extension(tmp_path):
+def create_tmp_extension(tmp_path: PurePath) -> Any:
     with open(os.path.join("data", "simpy.gram"), "r") as grammar_file:
-        grammar = grammar_file.read()
-    grammar = parse_string(grammar, GrammarParser)
+        grammar_source = grammar_file.read()
+    grammar = parse_string(grammar_source, GrammarParser)
     extension = generate_parser_c_extension(grammar, tmp_path)
     return extension
 
 
-def read_python_source(path):
+def read_python_source(path: str) -> str:
     with open(path, "r") as file:
         source = file.read()
     return source
 
 
-def test_ast_generation_on_source_files(tmp_path):
+def test_ast_generation_on_source_files(tmp_path: PurePath) -> None:
     extension = create_tmp_extension(tmp_path)
     for filename in PYTHON_SOURCE_FILENAMES:
         source = read_python_source(os.path.join("test", "test_data", filename))
