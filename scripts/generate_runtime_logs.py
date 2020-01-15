6a74c48439ad95fca0615d1f8e8f2978d100db48
Lysandros Nikolaou <lisandrosnik@gmail.com>
2020-01-15 04:54:53 +0100

    Add command line argument for cpython directory

--- scripts/generate_runtime_logs.py
+++ scripts/generate_runtime_logs.py
@@ -1,10 +1,23 @@
+import argparse
 import pathlib
 import subprocess
 
 import git
 
 
+argparser = argparse.ArgumentParser(
+    prog="generate_runtime_logs",
+    description="Helper program to generate runtime logs of running 'make simpy_cpython'",
+)
+argparser.add_argument("-c", "--cpython", help="The directory where CPython resides")
+
+
 def main() -> None:
+    args = argparser.parse_args()
+    cpython_dir = args.cpython or "../cpython"
+    lib_dir = str(pathlib.Path(cpython_dir).joinpath("Lib"))
+    print(lib_dir)
+
     repo = git.Repo(pathlib.Path.cwd())
     with open("simpy_cpython_timing.log", "w") as file:
         for commit in repo.iter_commits("80f8efb..master"):
@@ -12,7 +25,7 @@ def main() -> None:
             file.write(f"Commit: {commit.hexsha}\n")
             try:
                 output: str = subprocess.check_output(
-                    "make simpy_cpython CPYTHON=../cpython/Lib", shell=True
+                    f"make simpy_cpython CPYTHON={lib_dir}", shell=True
                 ).decode("utf-8")
                 file.write(f"{output.splitlines()[-2]}\n")
             except subprocess.CalledProcessError as e:
