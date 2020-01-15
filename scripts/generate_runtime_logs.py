13a29ba6d37a1a309b2e22afb0dc11aeeae6d644
Lysandros Nikolaou <lisandrosnik@gmail.com>
2020-01-15 04:33:50 +0100

    Catch CheckProcessError and log the return code

--- scripts/generate_runtime_logs.py
+++ scripts/generate_runtime_logs.py
@@ -9,11 +9,14 @@ def main() -> None:
     with open("simpy_cpython_timing.log", "w") as file:
         for commit in repo.iter_commits("80f8efb..master"):
             repo.head.reference = commit
-            output: str = subprocess.check_output(
-                "make simpy_cpython CPYTHON=../cpython/Lib", shell=True
-            ).decode("utf-8")
             file.write(f"Commit: {commit.hexsha}\n")
-            file.write(f"{output.splitlines()[-2]}\n")
+            try:
+                output: str = subprocess.check_output(
+                    "make simpy_cpython CPYTHON=../cpython/Lib", shell=True
+                ).decode("utf-8")
+                file.write(f"{output.splitlines()[-2]}\n")
+            except subprocess.CalledProcessError as e:
+                file.write(f"Exit status: {e.returncode}\n")
             file.write("\n")
     repo.head.reference = repo.heads.master
 
