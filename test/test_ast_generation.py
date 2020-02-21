043261b24cfb2bc96e9b7f9e4fc6ff566396f7da
Lysandros Nikolaou <lisandrosnik@gmail.com>
2020-02-21 05:45:20 +0100

    Remove lookaheads that are not needed anymore

--- test/test_ast_generation.py
+++ test/test_ast_generation.py
@@ -523,12 +523,14 @@ FAIL_TEST_CASES = [
     ("for_star_targets_subscript_call", "for a[b]() in c: pass"),
     ("for_star_targets_attribute_call", "for a.b() in c: pass"),
     ("for_star_targets_mixed_call", "for a[0].b().c.d() in e: pass"),
+    ("for_star_targets_in", "for a, in in b: pass"),
     ("f-string_assignment", "f'{x = 42}'"),
     ("f-string_empty", "f'{}'"),
     ("f-string_function_def", "f'{def f(): pass}'"),
     ("f-string_lambda", "f'{lambda x: 42}'"),
     ("f-string_singe_brace", "f'{'"),
     ("f-string_single_closing_brace", "f'}'"),
+    ("from_import_invalid", "from import import a"),
     # This test case checks error paths involving tokens with uninitialized
     # values of col_offset and end_col_offset.
     ("invalid indentation",
