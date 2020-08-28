ff6a5eeebd0187fa006220195fcdb21caaddd15b
Guido van Rossum <guido@python.org>
2020-08-28 15:19:18 -0700

    Add --start, --mode and --verify flags; refactoring under way

--- corpus.py
+++ corpus.py
@@ -25,7 +25,7 @@ import sys
 import tokenize
 import traceback
 
-from typing import Dict
+from typing import Dict, Optional, Tuple
 
 from pegen.testutil import recovery_by_insertions, describe_token
 from pegen.tokenizer import Tokenizer
@@ -36,8 +36,13 @@ argparser = argparse.ArgumentParser()
 argparser.add_argument(
     "-n", "--number", type=int, default=1, help="Number of cases to try (default 1; 0=all)"
 )
+argparser.add_argument("-s", "--start", type=int, default=0, help="First case to try (default 0)")
 argparser.add_argument(
-    "-s", "--start", type=int, default=0, help="First case to try (default 0)"
+    "-m",
+    "--mode",
+    default="all",
+    choices=("all", "indent", "syntax", "tab"),
+    help="What kind of cases to handle (default all)",
 )
 argparser.add_argument(
     "-d",
@@ -45,7 +50,19 @@ argparser.add_argument(
     default="data/parse_errors.json",
     help="JSON file containing the dataset (default data/parse_errors.json)",
 )
-# TODO: Arguments to select a single case, or a range, or a random sample
+argparser.add_argument(
+    "--verify",
+    action="store_true",
+    help="Print items in the dataset where ast.parse and the item disagree",
+)
+
+
+error_types = {
+    "all": ["IndentationError", "TabError", "SyntaxError"],
+    "indent": ["IndentationError"],
+    "tab": ["TabError"],
+    "syntax": ["SyntaxError"],
+}
 
 
 def main() -> None:
@@ -60,43 +77,137 @@ def main() -> None:
     number = min(number, len(keys) - start)
     if number <= 0:
         sys.exit(f"No cases selected; there are only {len(keys)} cases")
+    skip_counts: Dict[str, int] = {}
     error_counts: Dict[str, int] = {}
     for i in range(start, start + number):
         item = dataset[keys[i]]
         syntax_err_line_no = item["syntax_err_line_no"]
         error_type = item["error_type"]
-        ## if error_type != "SyntaxError":
-        ##     continue
+        if error_type not in error_types[args.mode]:
+            ## print(f"Skipping {error_type}", file=sys.stderr)
+            skip_counts[error_type] = skip_counts.get(error_type, 0) + 1
+            continue
         error_message = item["error_message"]
-        error_counts[error_type] = error_counts.get(error_type, 0) + 1
         content = item["content"]
-        print("=====", i, "=====")
+
+        if args.verify:
+            if compare_ast_errors(content, syntax_err_line_no, error_type, error_message):
+                ## print(f"{i}: all good", file=sys.stderr)
+                skip_counts[error_type] = skip_counts.get(error_type, 0) + 1
+                continue
+
+        error_counts[error_type] = error_counts.get(error_type, 0) + 1
+
+        print()
+        print(f"===== {i} =====")
         ## pprint.pprint(item)
         print(f"Line {syntax_err_line_no}: {error_type}: {error_message}")
         print("  +-------------------------")
         print(indent(content, syntax_err_line_no))
         print("  +-------------------------")
-        tester(content)
+        tester(content, try_ours=(not args.verify))
+        print()
 
-    print("",
-          "===== errors by type =====",
-          error_counts,
-          sep="\n", file=sys.stderr)
+    print("===== skips by type =====", skip_counts, sep="\n", file=sys.stderr)
+    print("===== errors by type =====", error_counts, sep="\n", file=sys.stderr)
 
 
-def tester(source: str) -> None:
-    # TODO: Be silent if (1) and (2) produce matching error locations.
+def compare_ast_errors(source: str, lineno: int, excname: str, message: str) -> bool:
+    err = try_ast_parse(source)
+    if err is None:
+        return False
+    if err.__class__.__name__ != excname:
+        # NOTE: "IndentationError: expected an indented block" is new, this
+        # used to be "SyntaxError: unexpected EOF while parsing".
+        if (
+            excname == "SyntaxError"
+            and message == "unexpected EOF while parsing"
+            and isinstance(err, IndentationError)
+            and err.msg == "expected an indented block"
+        ):
+            return True
+        return False
+    error_line = source.splitlines()[lineno - 1].strip()
 
+    # NOTE: If there's an error on a line with just a triple quote,
+    # the line numbers won't match because of difference in error
+    # reporting between 3.6 and 3.8+.
+    if error_line not in ('"""', "'''"):
+        if err.lineno != lineno:
+            return False
+
+    if err.msg == message:
+        return True
+
+    # NOTE: "can't" was replaced by "cannot" in Python 3.8.
+    if err.msg == message.replace("can't", "cannot"):
+        return True
+
+    # NOTE: "unmatched ']'" (or ')', or '}') is new, this used to be a
+    # plain "invalid syntax".  Ditto for "closing parenthesis 'x' does not
+    # match opening parenthesis 'u'.
+    if message == "invalid syntax" and (
+        err.msg.startswith("unmatched ") or err.msg.startswith("closing parenthesis ")
+    ):
+        return True
+
+    # NOTE: Python 3.8 reports more specifics for non-ASCII quotes.
+    if message == "invalid character in identifier" and err.msg.startswith("invalid character"):
+        return True
+
+    return False
+
+
+def compare_our_errors(source: str, ast_err: Exception) -> bool:
+    ## # 2) Pass it to our parser
+    ## err, parser = try_our_parser(source)
+    ## if err is None:
+    ##     return False
+    ## if err.__class__.__name__ != excname:
+    ##     # We don't always raise IndentationError or TabError where we should
+    ##     if excname in ("IndentationError", "TabError") and not isinstance(err, SyntaxError):
+    ##         return False
+    ## if err.lineno != lineno:
+    ##     return False
+    ## # Our message is almost always "pegen parse failure"
+
+    return True
+
+
+def tester(source: str, try_ours: bool) -> None:
     # 1) Pass it to ast.parse()
+    err = try_ast_parse(source)
+    if err is None:
+        print("ast.parse(): NO PROBLEM")
+    else:
+        print("ast.parse():")
+        print_exception(err)
+
+    if not try_ours:
+        return
+
+    # 2) Pass it to GeneratedParser
+    err, parser = try_our_parser(source)
+    if err is None:
+        print("our parser: NO PROBLEM")
+    else:
+        print("our parser:")
+        print_exception(err)
+
+        # 3) Try error correction
+        error_correction(parser)
+
+
+def try_ast_parse(source: str) -> Optional[Exception]:
     try:
         tree = ast.parse(source)
     except SyntaxError as err:
-        print("ast.parse():")
-        print_exception(err)
+        return err
     else:
-        print("ast.parse(): NO PROBLEM")
+        return None
 
-    # 2) Pass it to GeneratedParser
+
+def try_our_parser(source: str) -> Tuple[Optional[Exception], GeneratedParser]:
     file = io.StringIO(source)
     tokengen = tokenize.generate_tokens(file.readline)
     tokenizer = Tokenizer(tokengen)
@@ -104,19 +215,12 @@ def tester(source: str) -> None:
     try:
         tree = parser.file()
     except Exception as err:
-        print("our parser crashed:")
-        print_exception(err)
-        return
+        return err, parser
     if tree:
-        print("our parser: NO PROBLEM")
         ## pprint.pprint(tree)
+        return None, parser
     else:
-        print("our parser:")
-        print_exception(parser.make_syntax_error("<string>"))
-
-    # 3) Try error correction
-    if not tree:
-        error_correction(parser)
+        return parser.make_syntax_error("<string>"), parser
 
 
 def error_correction(parser: GeneratedParser) -> None:  # type: ignore[no-any-unimported]
@@ -137,7 +241,7 @@ def error_correction(parser: GeneratedParser) -> None:  # type: ignore[no-any-un
 
 
 def print_exception(err: Exception) -> None:
-        traceback.print_exception(err.__class__, err, None, file=sys.stdout)
+    traceback.print_exception(err.__class__, err, None, file=sys.stdout)
 
 
 def indent(content: str, lineno: int) -> str:
