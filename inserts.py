4628db15c9e1f045a240e57000281ae67d6aab84
Guido van Rossum <guido@python.org>
2020-08-28 17:32:17 -0700

    Add -c flag to inserts.py

--- inserts.py
+++ inserts.py
@@ -1,4 +1,5 @@
 import argparse
+import io
 import pprint
 import sys
 import token
@@ -14,21 +15,34 @@ from parse import GeneratedParser
 # A variation of simple_parser_main() in pegen/parser.py.
 def main() -> None:
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("filename", help="Input file ('-' to use stdin)")
+    argparser.add_argument("-c", dest="command", help="command to run (default read a file)")
+    argparser.add_argument("filename", nargs="?", help="Input file ('-' to use stdin)")
 
     args = argparser.parse_args()
 
-    filename = args.filename
-    if filename == "" or filename == "-":
-        filename = "<stdin>"
-        file = sys.stdin
+    if command := args.command:
+        if args.filename:
+            argparser.error("Use either -c or filename, not both")
+        filename = "<string>"
+        file = io.StringIO(command)
     else:
-        file = open(args.filename)
+        filename = args.filename
+        if not filename:
+            argparser.error("Exactly one of -c and filename is required")
+        if filename == "" or filename == "-":
+            filename = "<stdin>"
+            file = sys.stdin
+        else:
+            file = open(args.filename)
     try:
         tokengen = tokenize.generate_tokens(file.readline)
         tokenizer = Tokenizer(tokengen)
         parser = GeneratedParser(tokenizer)
-        tree = parser.file()
+        try:
+            tree = parser.file()
+        except Exception as err:
+            traceback.print_exception(err.__class__, err, None)
+            sys.exit(1)
         if not tree:
             err = parser.make_syntax_error(filename)
             traceback.print_exception(err.__class__, err, None)
